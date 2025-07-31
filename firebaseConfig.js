3d8a6b7c80448cc45c8d3328dc90f8b92603fe5a
Aris Al Hasan <arisalhasan@hotmail.com>
2025-07-31 23:49:46 +0300

    Switched to Firebase phone OTP auth and cleaned up code

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,24 @@
 // firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDqdcv_LEGiqvJrxomoHMO902Cx8zmiiVY",
   authDomain: "zeeliasbookings.firebaseapp.com",
   databaseURL: "https://zeeliasbookings-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "zeeliasbookings",
-  storageBucket: "zeeliasbookings.firebasestorage.app",
+  storageBucket: "zeeliasbookings.appspot.com",
   messagingSenderId: "551591035341",
   appId: "1:551591035341:web:4814897183d43f39b40484",
   measurementId: "G-VH4ER6881J"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+// Export Firestore instance
+export { db };
